ffd8fb2b92ba67ea330e6cef9130970502fc6f6b
fanyanggod <fanyanggod95@gmail.com>
2021-12-10 18:28:06 +0800

    feat: singleton mode

---
+++ src/com/sherlock/designpattern/singleton/LoggerUnSafe.java
@@ -0,0 +1,46 @@
+package com.sherlock.designpattern.singleton;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * @author sherlock
+ * @date 2021/12/10
+ */
+public class Test {
+    public static class Logger {
+        private  FileWriter writer;
+        public Logger(){
+            File file = new File("log file path");
+            try {
+                writer = new FileWriter(file, true);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void log(String message) {
+            try {
+                writer.write(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static class UserController{
+        private final Logger logger = new Logger();
+
+        public void create() {
+            logger.log("create user");
+        }
+    }
+
+    public static class OrderController{
+        private final Logger logger = new Logger();
+
+        public void create() {
+            logger.log("create Order");
+        }
+    }
+}
---
+++ src/com/sherlock/designpattern/singleton/StaticInternalSingleton.java
@@ -0,0 +1,5 @@
+package com.sherlock.designpattern.singleton;/**
+ *  @author sherlock
+ *  @date 2021/12/10
+ */public class StaticInternalSingleton {
+}
